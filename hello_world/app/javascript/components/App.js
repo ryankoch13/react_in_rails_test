1e78f123217e70b1e81a089d11201d295a6692f9
cynavago <cnavarrohdz@gmail.com>
2020-08-24 11:46:47 -0700

    completed react routing in rails

--- hello_world/app/javascript/components/App.js
+++ hello_world/app/javascript/components/App.js
@@ -1,11 +1,43 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React, { Component } from "react";
+import {
+  BrowserRouter as  Router,
+  Route,
+  Switch,
+  NavLink
+} from 'react-router-dom';
+
+import AboutMe from './pages/AboutMe'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import {
+  Nav, 
+  NavItem
+} from 'reactstrap'
+
+import PropTypes from "prop-types";
+
 class App extends React.Component {
   render() {
     return ( 
-    <React.Fragment>
-      <h1> Hello, World! </h1>
-      </React.Fragment>
+     <Router>
+       <Nav>
+         <NavItem>
+           <NavLink to="/" >Home</NavLink>
+         </NavItem>
+         <NavItem>
+           <NavLink to="/AboutMe" >About Me</NavLink>
+         </NavItem>
+         <NavItem>
+           <NavLink to="/Projects" >Projects</NavLink>
+         </NavItem>
+       </Nav>
+       
+       <Switch>
+         <Route exact path="/" component={ Home } />
+         <Route path="/AboutMe" component={ AboutMe } />
+         <Route path="/Projects" component={ Projects } />
+       </Switch>
+     </Router>
     );
   }
 }
---
+++ hello_world/app/javascript/components/pages/AboutMe.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+class AboutMe extends React.Component {
+  render() {
+    return(
+      <h3>About me</h3>
+    )
+  }
+}
+
+export default AboutMe
